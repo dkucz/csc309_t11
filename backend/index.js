5f3e3d7967fea807a545d6e975017635218b5af1
Dominik Kucz <dominik.kucz@mail.utoronto.ca>
2026-03-29 20:28:52 -0400

    trying to fix cors

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,11 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 const app = express();
 
-// TODO: complete me (CORS)
-app.use(express.json());
 app.use(cors({
     origin: [FRONTEND_URL, "http://localhost:5173"]
 }));
+
+// TODO: complete me (CORS)
+app.use(express.json());
 app.use('', routes);
 export default app;
